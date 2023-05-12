ca1cf8ceaeb5cc72ce17a3df38df16c4d783245c
Jen Ziel <jenziel400@gmail.com>
2023-05-12 19:36:56 -0400

    complete responses for testing

--- src/order.js
+++ src/order.js
@@ -1,7 +1,51 @@
+function takeOrder(order1, deliveryOrders) {
+  if(deliveryOrders.length < 3)
+  return  deliveryOrders.push(order1)
+  }
+
+
+
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++)
+  if (deliveryOrders[i].orderNumber === orderNumber) {
+    return deliveryOrders.splice([i], 1) 
+  }
+
+}
+function listItems(deliveryOrders) {
+  var newArray = []
+  for (var i = 0; i < deliveryOrders.length; i++) {
+  newArray.push(deliveryOrders[i].item)
+} 
+return newArray.join(', ')
+}
+
+// go through each object in the array, 
+//check to see if that item is in the list
+
+function searchOrder(deliveryOrders, item1) {
+  var emptyArray = []
+for (var i = 0; i < deliveryOrders.length ; i++) {
+  emptyArray.push(deliveryOrders[i].item)
+  emptyArray.join(', ')
+} {
+  if (emptyArray.includes(item1)) {
+  return true
+} else {
+  return false
+}
+}
+}
+ 
+
+//  if (deliveryOrders[i].item === item1) 
+//   return true
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
