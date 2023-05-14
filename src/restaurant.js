556e5787854fb9268f5a7b5b51bdcc955d9e6368
Jen Ziel <jenziel400@gmail.com>
2023-05-14 18:34:52 -0400

    add successful completion of checkForFood function

--- src/restaurant.js
+++ src/restaurant.js
@@ -33,45 +33,94 @@ function createRestaurant(nameOfRestaurant){
 function addMenuItem(restaurant, newMenuItem) {
   if (newMenuItem.type === "breakfast" && !restaurant.menus.breakfast.includes(newMenuItem)){
     restaurant.menus.breakfast.push(newMenuItem)
-    return restaurant.menus.breakfast
+    // return restaurant.menus.breakfast
   } else if (newMenuItem.type === "lunch" && !restaurant.menus.lunch.includes(newMenuItem)) {
     restaurant.menus.lunch.push(newMenuItem)
-    return restaurant.menus.lunch
+    // console.log()
+    // return restaurant.menus.lunch
   } else if (newMenuItem.type === "dinner" && !restaurant.menus.dinner.includes(newMenuItem)) {
     restaurant.menus.dinner.push(newMenuItem)
-    return restaurant.menus.dinner
+    // return restaurant.menus.dinner
   }
+  console.log("restaurant.menus: ", restaurant.menus)
+  return restaurant.menus
+  
 }
   
-
- 
- 
+// if (!restaurant.menus.breakfast.includes(existingMenuItem) && !restaurant.menus.lunch.includes(existingMenuItem) && !restaurant.menus.dinner.includes(existingMenuItem )) {
+//   return error1} 
 function removeMenuItem(restaurant, existingMenuItem, whichMenu){
-  console.log("restaurant: ", restaurant)
-
+  
+  var error1 = `Sorry, we don't sell ${existingMenuItem}, try adding a new recipe!`
   if(whichMenu === "breakfast") {
-    restaurant = restaurant.menus.breakfast
-  } else if (whichMenu ==="lunch") {
-    restaurant = restaurant.menus.lunch
-  } else if (whichMenu ==="dinner") {
-    restaurant = restaurant.menus.dinner
+    restaurant = restaurant.menus.breakfast;
   }
+  if (whichMenu ==="lunch") {
+    restaurant = restaurant.menus.lunch
+  }
+  if (whichMenu ==="dinner" ) {
+    restaurant = restaurant.menus.dinner
+  } 
 var itemToFind = existingMenuItem
 var foundIndex = restaurant.findIndex(el =>  el == itemToFind)
 restaurant.splice(foundIndex, 1)
-restaurant = 
-console.log("restaurant ", restaurant)
 return  `No one is eating our ${existingMenuItem} - it has been removed from the ${whichMenu} menu!`
 }
 
+// //Passes iteration 1:
+// function checkForFood(restaurant, foodItem) {
+//   return `Yes, we're serving ${foodItem.name} today!`
+// }
 
-function checkForFood(){
+// // passes iteration 1 & 2: 
+// function checkForFood(restaurant, foodItem) {
+//   console.log("print out restaurant array: ", restaurant)
+//   // for (var i = 0; i < restaurant.menus; i++)
+//   if (restaurant.menus.breakfast.includes(foodItem) || restaurant.menus.lunch.includes(foodItem) || restaurant.menus.dinner.includes(foodItem)) {
+//     return `Yes, we're serving ${foodItem.name} today!`
+//   }
+//  }
 
+// passes iteration 1 & 2: 
+function checkForFood(restaurant, foodItem) {
+  var unavailableResponse = `Sorry, we aren't serving ${foodItem.name} today.`
+  var weHaveIt = false;
+  for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
+    console.log("restaurant.menus: ", restaurant.menus)
+    if (restaurant.menus.breakfast[i] === foodItem){
+      weHaveIt = true
+      }
+  }
+  for (var i = 0; i < restaurant.menus.lunch.length; i++) {
+    if (restaurant.menus.lunch[i] === foodItem) {
+    weHaveIt = true
+  }
+  }
+  for (var i = 0; i < restaurant.menus.dinner.length; i++) {
+    if ( restaurant.menus.dinner[i] === foodItem)  {
+      weHaveIt = true
+    }
+  }
+    if (weHaveIt === false){
+      return unavailableResponse
+    } else return`Yes, we're serving ${foodItem.name} today!`
 }
+
+
+
+//  // iteration 3:
+// function checkForFood(restaurant, foodItem){
+//   var unavailableResponse = `Sorry, we aren't serving ${foodItem.name} today.`
+//   return unavailableResponse
+// }
+
+// // iteration 4:
+// function checkForFood(restaurant, foodItem){
+// }
 
 module.exports = {
   createRestaurant, 
   addMenuItem,
   removeMenuItem,
-  checkForFood
+  checkForFood,
 }
