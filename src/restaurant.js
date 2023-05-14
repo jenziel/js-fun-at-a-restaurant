68e412929a58a3d29e4ac7e770e32a2519acb4c6
Jen Ziel <jenziel400@gmail.com>
2023-05-13 23:18:13 -0400

    pass the first 3 tests in the add menu items tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,9 +1,8 @@
 function createRestaurant(nameOfRestaurant){
  var restaurant = {
-    menus:{breakfast : [], lunch: [], dinner: []}
+    menus:{breakfast: [], lunch: [], dinner: []}
  }
- 
- restaurant.name = nameOfRestaurant
+  restaurant.name = nameOfRestaurant
 
  console.log(restaurant)
  console.log(restaurant.menus)
@@ -11,12 +10,27 @@ function createRestaurant(nameOfRestaurant){
  console.log(restaurant)
  }
  
-
-
-function addMenuItem(){
-
+//  function addMenuItem(restaurant, newLunchItem){
+//  restaurant.menus.lunch[0] = newLunchItem
+//  return restaurant.menus[0]
+// }
+ 
+function addMenuItem(restaurant, newMenuItem) {
+  if (newMenuItem.type === "breakfast" ){
+    restaurant.menus.breakfast.push(newMenuItem)
+    return restaurant.menus.breakfast
+  } else if (newMenuItem.type === "lunch") {
+    restaurant.menus.lunch.push(newMenuItem)
+    return restaurant.menus.lunch
+  } else if (newMenuItem.type === "dinner") {
+    restaurant.menus.dinner.push(newMenuItem)
+    return restaurant.menus.dinner
+  }
 }
+  
 
+ 
+ 
 function removeMenuItem(){
 
 }
